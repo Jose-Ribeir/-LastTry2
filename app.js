73aa1a9c6553ce2d4c742334de776fad2800377e
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-07 14:45:06 +0100

    final commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+const bodyParser = require('body-parser')
+const users=require('./routes/user')
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+
+const stores = require('./routes/store')
+const cfg = require('./routes/cfg')
+const software=require('./routes/software')
+
+//user
+app.get('/users', users.getUsers)
+app.get('/users/:id', users.getUserById)
+app.post('/users', users.createUser)
+app.post('/login', users.getLogin)
+app.put('/users/:id', users.updateUser)
+app.delete('/users/:id', users.deleteUser)
+
+
+//cfg
+app.get('/cfg', cfg.getCfg)
+app.get('/cfg/:id', cfg.getCfgById)
+app.post('/cfg', cfg.createCfg)
+app.delete('/cfg/:id', cfg.deleteCfg)
+app.get('/cfg/software/:id', cfg.getCfgBySoftwareId)
+
+//store
+app.get('/stores', stores.getStores)
+
+//software
+app.get('/software/games',software.getGames)
+app.get('/software/apps',software.getApps)
+
+
+
+
+module.exports = app;
---
+++ routes/cfg.js
@@ -0,0 +1,72 @@
+const client = require('../../untitled3/models/connection')
+const express = require('express');
+const {log} = require("debug");
+const {md5} = require("pg/lib/utils");
+const app = express();
+
+client.connect();
+
+const getCfg = (request, response) => {
+    client.query('SELECT * FROM cfg', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getCfgBySoftwareId = (request, response) => {
+    const id = parseInt(request.params.id)
+    client.query('Select * from cfg inner join software s on cfg.cfg_software_id = s.software_id where cfg_software_id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+
+const createCfg = (request, response) => {
+    const cfg = request.body
+    client.query('INSERT INTO cfg (cfg_name) VALUES ($1)', [cfg.cfg_name.toString()], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`cfg added`)
+    })
+}
+
+const getCfgById = (request, response) => {
+    const id = parseInt(request.params.id)
+    client.query('SELECT * FROM cfg WHERE cfg_id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        } client.query('UPDATE cfg SET cfg_view = cfg_view + 1 WHERE cfg_id = $1',[id], (error, results) =>{
+            if(error){
+                throw error
+            }
+            response.status(200).json(results.rows)
+        })
+
+        response.status(200).json(results.rows)
+    })
+}
+
+const deleteCfg = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    client.query('DELETE FROM cfg WHERE cfg_id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`cfg deleted with ID: ${id}`)
+    })
+}
+
+module.exports = {
+    getCfg,
+    getCfgById,
+    createCfg,
+    deleteCfg,
+    getCfgBySoftwareId
+}
