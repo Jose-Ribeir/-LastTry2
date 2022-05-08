d25f72d75781a2627fd1836cea1de8179b4177d3
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-08 19:36:08 +0100

    change password html

--- public/Dashboard/js/formSender.js
+++ public/Dashboard/js/formSender.js
@@ -37,6 +37,40 @@ async function upDate() {
     }
 }
 
+async function changePass() {
+    if (document.getElementById('newpass2').value === document.getElementById('newpass1').value){
+    let data = {"person_id": "1",
+        "person_password": document.getElementById('oldpass').value,
+        "person_passwordnew":  document.getElementById('newpass1').value};
+
+    try {
+
+        //get json here
+        let newProduct = await $.ajax({
+            url: "https://cfg-api-ultimate.herokuapp.com/changepass",
+            method: "put",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json"
+        });
+        alert(JSON.stringify(newProduct))
+
+        sessionStorage.setItem("user_id",JSON.stringify(newProduct.person_id))
+        window.location.href='../../Dashboard/table.html'
+
+    } catch (err) {
+        console.log(err);
+        if (err.responseJSON) {
+            alert(""+err.responseJSON.msg);
+        } else {
+            alert("Was not able to add product") ;
+        }
+    }
+    }
+    else
+        alert("Passwords don't coincide")
+}
+
 
 
 
@@ -98,8 +132,11 @@ window.onload = async function() {
     document.getElementById("inputBio").value= ""+json[0].person_bio;
 
 
+}
 
 
 
+async function changePass(){
+   window.location.href="../Dashboard/changepass.html"
 
 }
