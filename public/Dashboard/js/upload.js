d16973ce5708663cfde6ebdad771cdbd09c55149
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-10 16:55:07 +0100

    select game from database

--- public/Dashboard/js/upload.js
+++ public/Dashboard/js/upload.js
@@ -1,42 +1,50 @@
-import http from "http";
-import formidable from "formidable";
-import fs from "fs";
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
+//
+// function readURL(input) {
+//     if (input.files && input.files[0]) {
+//
+//         var reader = new FileReader();
+//
+//         reader.onload = function(e) {
+//             $('.image-upload-wrap').hide();
+//
+//             $('.file-upload-image').attr('src', e.target.result);
+//             $('.file-upload-content').show();
+//
+//             $('.image-title').html(input.files[0].name);
+//         };
+//
+//         reader.readAsDataURL(input.files[0]);
+//
+//     } else {
+//         removeUpload();
+//     }
+// }
+//
+// function removeUpload() {
+//     $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+//     $('.file-upload-content').hide();
+//     $('.image-upload-wrap').show();
+// }
+// $('.image-upload-wrap').bind('dragover', function () {
+//     $('.image-upload-wrap').addClass('image-dropping');
+// });
+// $('.image-upload-wrap').bind('dragleave', function () {
+//     $('.image-upload-wrap').removeClass('image-dropping');
+// });
 
-        var reader = new FileReader();
 
-        reader.onload = function(e) {
-            $('.image-upload-wrap').hide();
+let software1="da"
+async function getData(id){
 
-            $('.file-upload-image').attr('src', e.target.result);
-            $('.file-upload-content').show();
-
-            $('.image-title').html(input.files[0].name);
-        };
-
-        reader.readAsDataURL(input.files[0]);
-
-    } else {
-        removeUpload();
+    if (id===1){
+        software1="apps"
     }
-}
-
-function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.file-upload-content').hide();
-    $('.image-upload-wrap').show();
-}
-$('.image-upload-wrap').bind('dragover', function () {
-    $('.image-upload-wrap').addClass('image-dropping');
-});
-$('.image-upload-wrap').bind('dragleave', function () {
-    $('.image-upload-wrap').removeClass('image-dropping');
-});
+    if (id===0){
+        software1="games"
+    }
 
 
-async function getData(){
     // var targetUrl = 'https://cfg-api-ultimate.herokuapp.com/users'
     //
     //
@@ -47,33 +55,51 @@ async function getData(){
 
 
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/users/'+id
+        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/software/'+software1
 
     const response = await fetch(
-        targetUrl)
+        proxyUrl+targetUrl)
     const data = await response.json()
     return data
 
 }
-let gameorsoftware=0
-function iconChange(){
+let clean
 
-    gameorsoftware=1
-    let a =document.getElementById("ButonisGame")
-    let b =document.getElementById("button")
-    if(gameorsoftware === 0){
-        gameorsoftware++
-        a.remove();
-        document.getElementById("img").remove()
-        b.innerHTML="<img id='img' src=\"images/icon/logo.png\" height=\"75\" width=\"75\"/>"
 
+async function fillDropdown() {
+    let field = document.getElementById("dropDown")
+    let json = await getData(gameorsoftware)
+    clean=json.length
+        field.innerHTML = '<option>Select</option>'
+
+    for (let i = 0; i < json.length; i++) {
+        field.innerHTML += '<option>' + json[i].software_name + '</option>'
     }
 
-    if(gameorsoftware === 1){
-        gameorsoftware--
-        a.remove();
-        document.getElementById("img").remove()
-        b.innerHTML="<img id='img'src=\"images/icon/logogaming.png\" height=\"75\" width=\"75\"/>"
+}
+
+
+
+let gameorsoftware=1
+async function iconChange() {
+    let a = document.getElementById("ButonisGame")
+    let b = document.getElementById("button")
+    if (gameorsoftware === 0) {
+        a.innerText = "Software"
+        b.innerHTML = "<img id='img' src=\"images/icon/logo.png\" height=\"75\" width=\"75\"/>"
+        gameorsoftware = 1
+    } else {
+        if (gameorsoftware === 1) {
+            a.innerText = "Game"
+            b.innerHTML = "<img id='img'src=\"images/icon/logogaming.png\" height=\"75\" width=\"75\"/>"
+            gameorsoftware = 0
+        }
     }
+    await fillDropdown()
+}
+window.onload = async function() {
+
+    await fillDropdown()
+
 
 }
