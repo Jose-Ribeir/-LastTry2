234e61e1c84a553f0845c7b96270e7f38f174f22
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-12 19:29:32 +0100

    create user inicio

--- public/Dashboard/js/register.js
+++ public/Dashboard/js/register.js
@@ -5,7 +5,7 @@ var temp;
 async function add(){
     await newUser()
     alert("Account created")
-    window.location.href='../Dashboard/Profile'
+    window.location.href='../Dashboard/Profile.html'
 }
 
 
