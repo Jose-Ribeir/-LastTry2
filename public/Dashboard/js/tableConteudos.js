c842ab0d217c323716ab09ba75e5693671026b00
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-15 23:50:37 +0100

    site bonito shee

--- public/Dashboard/js/tableConteudos.js
+++ public/Dashboard/js/tableConteudos.js
@@ -12,7 +12,7 @@ async function getData(){
         targetUrl = 'https://cfg-api-ultimate.herokuapp.com/cfg'
 
     const response = await fetch(
-        proxyUrl + targetUrl)
+         targetUrl)
     const data = await response.json()
     return data
 }
