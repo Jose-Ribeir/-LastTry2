354982ade6fe7d5bf6d8199d59a38de768206ce1
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-17 18:45:11 +0100

    site bonito shee

--- public/gaming.js
+++ public/gaming.js
@@ -6,16 +6,16 @@ async function getData(){
     // const data = await response.json()
     // console.log(data)
     // return data
-    alert("getdata")
+
     let type=queryString.substring(1,queryString.length)
 
-    alert("Type= "+type)
+
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
         targetUrl = 'https://cfg-api-ultimate.herokuapp.com/software/'+type
     const response = await fetch(
         targetUrl)
     const data = await response.json()
-    alert(""+data[0])
+
     return data
 }
 
@@ -26,7 +26,6 @@ var queryString
 window.onload = async function() {
     queryString = window.location.search;
     console.log(queryString);
-    alert(""+queryString)
 
     const json = await getData()
     alert(""+json[0])
@@ -34,7 +33,7 @@ window.onload = async function() {
     for (let i = 0; i < json.length; i++) {
         lista.innerHTML+="<div class=\"u-container-style u-list-item u-repeater-item\">\n" +
             "              <div class=\"u-container-layout u-similar-container u-container-layout-1\">\n" +
-            "                <p class=\"u-text u-text-1\">"+json[i]+"</p>\n" +
+            "                <p class=\"u-text u-text-1\">"+json[i].software_name+"</p>\n" +
             "                <img class=\"u-hover-feature u-image u-image-default u-preserve-proportions u-image-1\" src=\"images/272f78_95999562a4cd4ea08798d569018146bd_mv2.png\" alt=\"\" data-image-width=\"78\" data-image-height=\"78\">\n" +
             "              </div>\n" +
             "            </div>"
