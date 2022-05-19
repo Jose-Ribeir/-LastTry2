f4f609ad0fca4be320cecfc4eaee6e41ad18b2c8
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-19 11:47:29 +0100

    nav between pages with arguments passage

--- public/software.js
+++ public/software.js
@@ -60,7 +60,7 @@ window.onload = async function() {
 
     alert(""+type)
    let software= getSoftware(type)
-    alert(""+software[0].software_name)
+    alert(""+software[0])
 
     document.getElementById("software").innerHTML=' <img id="softwareImg" class="u-image u-align-center u-image-1" src="images/stsmall845x845-pad1000x1000f8f8f8-12.jpg" data-image-width="1000" data-image-height="1000"><h2 id="softwareName" class="u-custom-font u-font-ubuntu u-text u-text-default u-text-1">'+software[0].software_name+'</h2>'
 
