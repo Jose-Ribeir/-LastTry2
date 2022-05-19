d2d346fe94333969974fb0489de0000d4ac425c6
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-19 09:15:42 +0100

    nav between pages with arguments passage

--- public/software.js
+++ public/software.js
@@ -19,27 +19,8 @@ async function getData(){
     return data
 }
 
-
-
-
-
-
-
-
-
-
-async function click(id) {
-
-
-}
-
-
-
-
-
-async function batata() {
-    alert("batata")
-  const  queryString = window.location.search;
+window.onload = async function() {
+    const  queryString = window.location.search;
     console.log(queryString);
 
 
@@ -47,25 +28,10 @@ async function batata() {
     alert(""+json[0])
     let lista=document.getElementById("list")
     for (let i = 0; i < json.length; i++) {
-        lista.innerHTML+= '<div class="u-align-left u-container-style u-layout-cell u-size-20 u-layout-cell-1">\n' +
-            '                        <div class="u-container-layout u-valign-top u-container-layout-1">\n' +
-            '                            <h4 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-3">Sample Headline</h4>\n' +
-            '                            <p class="u-custom-font u-font-ubuntu u-text u-text-4">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>\n' +
-            '                        </div>\n' +
-            '                    </div>'
+        lista.innerHTML+= '<div class="u-align-left u-container-style u-layout-cell u-size-20 u-layout-cell-1"><div class="u-container-layout u-valign-top u-container-layout-1"><h4 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-3">'+json[i].cfg_name+'</h4><p class="u-custom-font u-font-ubuntu u-text u-text-4">'+json[i].cfg_description+'</p></div></div>'
     }
-
 }
-
-function f() {
-    alert("Function f")
-}
-window.onload = async function() {
-    alert("Onloadddd")
-    batata()
-
-}
-window.addEventListener("load", async function () {
-    alert("event listener")
-    batata()
-})
+// window.addEventListener("load", async function () {
+//     alert("event listener")
+//     batata()
+// })
