6e76e0871508ed8e646f4c731aca446baf5039ec
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-19 09:55:46 +0100

    nav between pages with arguments passage

--- public/software.js
+++ public/software.js
@@ -1,4 +1,4 @@
-async function getData(){
+async function getCfgs(a){
     // var targetUrl = 'https://cfg-api-ultimate.herokuapp.com/users'
     //
     //
@@ -7,38 +7,58 @@ async function getData(){
     // console.log(data)
     // return data
 
-    let type=queryString.substring(1,queryString.length)
-    type=type.substring(0,type.indexOf("&"))
-    alert(type)
+
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/cfg/'+type
+        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/cfg/'+a
 
     const response = await fetch(
         targetUrl)
     const data = await response.json()
     return data
 }
- var queryString
+
+
+
+
+
+async function getSoftware(a){
+    // var targetUrl = 'https://cfg-api-ultimate.herokuapp.com/users'
+    //
+    //
+    // const response = await fetch(targetUrl)
+    // const data = await response.json()
+    // console.log(data)
+    // return data
+
+
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/software/'+a
+
+    const response = await fetch(
+        targetUrl)
+    const data = await response.json()
+    return data
+}
+
+
+
+
+
 window.onload = async function() {
-    alert("onload")
+
     queryString = window.location.search;
-    alert(""+queryString)
-    console.log(queryString);
 
+    let type=queryString.substring(1,queryString.length)
+    type=type.substring(0,type.indexOf("&"))
 
-    const json = await getData()
+    const json = await getCfgs(type)
     alert(""+json[0])
     let lista=document.getElementById("list")
     for (let i = 0; i < json.length; i++) {
         lista.innerHTML+= '<div class="u-align-left u-container-style u-layout-cell u-size-20 u-layout-cell-1"><div class="u-container-layout u-valign-top u-container-layout-1"><h4 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-3">'+json[i].cfg_name+'</h4><p class="u-custom-font u-font-ubuntu u-text u-text-4">'+json[i].cfg_description+'</p></div></div>'
     }
 
-    let type=queryString.substring(1,queryString.length)
-    type=type.substring(type.indexOf("&")+1, type.length)
+   let software= getSoftware(type)
 
-    document.getElementById("softwareName").innerHTML=''+type
+    document.getElementById("software").innerHTML=' <img id="softwareImg" class="u-image u-align-center u-image-1" src="'+software[0].software_image+'" data-image-width="1000" data-image-height="1000"><h2 id="softwareName" class="u-custom-font u-font-ubuntu u-text u-text-default u-text-1">'+software[0].software_name+'</h2>'
 }
-// window.addEventListener("load", async function () {
-//     alert("event listener")
-//     batata()
-// })
