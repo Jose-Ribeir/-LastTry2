a5bc63ff42dc570665e70f2e323a46f0e6aa6877
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-19 12:38:47 +0100

    Pagina de software com click na cfg a funcionar

--- public/software.js
+++ public/software.js
@@ -44,7 +44,6 @@ async function getSoftware(a){
 
 
 function teste(a) {
-    alert(a)
     window.location.href='config.html?'+a
 }
 
@@ -59,7 +58,7 @@ window.onload = async function() {
 
     let lista=document.getElementById("list")
     for (let i = 0; i < json.length; i++) {
-        lista.innerHTML+= '<div class="u-align-left u-container-style u-layout-cell u-size-20 u-layout-cell-1"><div onclick="'+teste(this.id)+'" id="'+json[i].cfg_id+'&'+json[i].cfg_name+'" class="u-container-layout u-valign-top u-container-layout-1"><h4 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-3">'+json[i].cfg_name+'</h4><p class="u-custom-font u-font-ubuntu u-text u-text-4">'+json[i].cfg_description+'</p></div></div>'
+        lista.innerHTML+= '<div class="u-align-left u-container-style u-layout-cell u-size-20 u-layout-cell-1"><div onclick="teste(this.id)" id="'+json[i].cfg_id+'&'+json[i].cfg_name+'" class="u-container-layout u-valign-top u-container-layout-1"><h4 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-3">'+json[i].cfg_name+'</h4><p class="u-custom-font u-font-ubuntu u-text u-text-4">'+json[i].cfg_description+'</p></div></div>'
     }
 
    let software= await getSoftware(type)
