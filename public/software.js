f785e4a1c12f1da5663493f0935fc6bbcf954523
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-19 09:37:00 +0100

    nav between pages with arguments passage

--- public/software.js
+++ public/software.js
@@ -32,6 +32,11 @@ window.onload = async function() {
     for (let i = 0; i < json.length; i++) {
         lista.innerHTML+= '<div class="u-align-left u-container-style u-layout-cell u-size-20 u-layout-cell-1"><div class="u-container-layout u-valign-top u-container-layout-1"><h4 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-3">'+json[i].cfg_name+'</h4><p class="u-custom-font u-font-ubuntu u-text u-text-4">'+json[i].cfg_description+'</p></div></div>'
     }
+
+    let type=queryString.substring(1,queryString.length)
+    type=type.substring(type.indexOf("&"), type.length)
+
+    document.getElementById("softwareName").innerHTML=''+type
 }
 // window.addEventListener("load", async function () {
 //     alert("event listener")
