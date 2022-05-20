12c304a3e3433de81c220c978bda15ec7b2f2320
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-20 10:55:37 +0100

    image from data base

--- public/gaming.js
+++ public/gaming.js
@@ -35,5 +35,4 @@ window.onload = async function() {
     for (let i = 0; i < json.length; i++) {
         lista.innerHTML+= '<div class="u-container-style u-list-item u-repeater-item"> <div onclick="teste(this.id)" id="'+json[i].software_id+'&'+json[i].software_name+'" class="u-container-layout u-similar-container u-container-layout-1"> <p class="u-text u-text-1">'+json[i].software_name+'</p> <img  class="u-hover-feature u-image u-image-default u-preserve-proportions u-image-1" src="images/'+json[i].software_image+'" alt="" data-image-width="78" data-image-height="78"> </div> </div>'
     }
-
 }
