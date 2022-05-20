a91d4fa9bf565bb81c98b1646fe70f265c5e98ee
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-20 10:55:17 +0100

    image working

--- public/gaming.js
+++ public/gaming.js
@@ -33,7 +33,7 @@ window.onload = async function() {
     alert(""+json[0])
     let lista=document.getElementById("list")
     for (let i = 0; i < json.length; i++) {
-        lista.innerHTML+= '<div class="u-container-style u-list-item u-repeater-item"> <div onclick="teste(this.id)" id="'+json[i].software_id+'&'+json[i].software_name+'" class="u-container-layout u-similar-container u-container-layout-1"> <p class="u-text u-text-1">'+json[i].software_name+'</p> <img  class="u-hover-feature u-image u-image-default u-preserve-proportions u-image-1" src="images/272f78_95999562a4cd4ea08798d569018146bd_mv2.png" alt="" data-image-width="78" data-image-height="78"> </div> </div>'
+        lista.innerHTML+= '<div class="u-container-style u-list-item u-repeater-item"> <div onclick="teste(this.id)" id="'+json[i].software_id+'&'+json[i].software_name+'" class="u-container-layout u-similar-container u-container-layout-1"> <p class="u-text u-text-1">'+json[i].software_name+'</p> <img  class="u-hover-feature u-image u-image-default u-preserve-proportions u-image-1" src="images/'+json[i].software_image+'" alt="" data-image-width="78" data-image-height="78"> </div> </div>'
     }
 
 }
