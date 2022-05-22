fc2bf03369c612be76048265c4063c30214ed3c4
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-22 18:52:04 +0100

    map on cfg page with circle

--- public/config.js
+++ public/config.js
@@ -1,55 +1,4 @@
-
-async function getcfg(){
-    // var targetUrl = 'https://cfg-api-ultimate.herokuapp.com/users'
-    //
-    //
-    // const response = await fetch(targetUrl)ff
-    // const data = await response.json()
-    // console.log(data)
-    // return data
-
-    let type=queryString.substring(1,queryString.length)
-    type=type.substring(0,type.indexOf("&"))
-
-    alert(""+type)
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/cfg/'+type
-    const response = await fetch(
-        targetUrl)
-    const data = await response.json()
-
-    return data
-}
-
-
-
-var queryString
-
-window.onload = async function() {
-    queryString = window.location.search;
-    console.log(queryString);
-    const json = await getcfg()
-
-    alert("Json getData config"+JSON.stringify(json))
-    document.getElementById("descri").innerHTML=""+json[0].cfg_description
-    document.getElementById("date").innerHTML=""+json[0].cfg_date
-    document.getElementById("cfgName").innerHTML=""+json[0].cfg_name
-    document.getElementById("keyact").innerHTML=""+json[0].cfg_key_action
-    document.getElementById("person").innerHTML=""+json[0].person_name+" "+json[0].person_surname
-    document.getElementById("views").innerHTML=""+json[0].cfg_view
-    document.getElementById("gameName").innerHTML=""+json[0].software_name
-    document.getElementById("softwareImg").src="images/"+json[0].software_image
-
-    // document.getElementById("keyact").value=json.cfg_
-
-
-
-}
-
-
 // Mapa do circlo
-
-
 
 const citymap = {
     lisboa: {
@@ -186,24 +135,62 @@ async function initMap() {
 
     }
 }
+window.initMap = initMap;
 
-async function getData(){
-    // var targetUrl = 'https://cfg-api-ultimate.herokuapp.com/stores'
+
+
+
+
+
+
+
+
+
+async function getcfg(){
+    // var targetUrl = 'https://cfg-api-ultimate.herokuapp.com/users'
     //
     //
-    // const response = await fetch(targetUrl)
+    // const response = await fetch(targetUrl)ff
     // const data = await response.json()
     // console.log(data)
     // return data
 
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/stores'
+    let type=queryString.substring(1,queryString.length)
+    type=type.substring(0,type.indexOf("&"))
 
+    alert(""+type)
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/cfg/'+type
     const response = await fetch(
-        proxyUrl + targetUrl)
+        targetUrl)
     const data = await response.json()
+
     return data
+}
+
+
+
+var queryString
+
+window.onload = async function() {
+    queryString = window.location.search;
+    console.log(queryString);
+    const json = await getcfg()
+
+    alert("Json getData config"+JSON.stringify(json))
+    document.getElementById("descri").innerHTML=""+json[0].cfg_description
+    document.getElementById("date").innerHTML=""+json[0].cfg_date
+    document.getElementById("cfgName").innerHTML=""+json[0].cfg_name
+    document.getElementById("keyact").innerHTML=""+json[0].cfg_key_action
+    document.getElementById("person").innerHTML=""+json[0].person_name+" "+json[0].person_surname
+    document.getElementById("views").innerHTML=""+json[0].cfg_view
+    document.getElementById("gameName").innerHTML=""+json[0].software_name
+    document.getElementById("softwareImg").src="images/"+json[0].software_image
+
+    // document.getElementById("keyact").value=json.cfg_
+
+
 
 }
 
-window.initMap = initMap;
+
