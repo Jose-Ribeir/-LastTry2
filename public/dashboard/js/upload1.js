c9c71a6d37f9acc44bee48599882eb66a76be0a0
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-23 18:51:31 +0100

    tentativa de upload de cfg

--- public/dashboard/js/upload1.js
+++ public/dashboard/js/upload1.js
@@ -6,3 +6,25 @@ async function uploadFile() {
         body: formData
     });
 }
+
+$(document).ready(function (e) {
+    $("#form").on('submit', (function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "https://cfg-api-ultimate.herokuapp.com/fileupload",
+            type: "POST",
+            data: new FormData(this),
+            dataType: 'json',
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    }));
+});
