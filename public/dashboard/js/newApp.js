27bad6bafd195bd7dde92ead6a36295de12254c8
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-24 17:35:09 +0100

    New page add software

--- public/dashboard/js/newApp.js
+++ public/dashboard/js/newApp.js
@@ -2,35 +2,40 @@ async function uploadApp() {
     const d = new Date();
     let field = document.getElementById("type")
     let isgame
-    if(field.innerText==="Game")
+    if(field.innerText==="Game"){
         isgame=true
-    if(field.innerText==="Application")
+    }
+
+    if(field.innerText==="Application"){
         isgame=false
+    }
 
-            let data = {"software_name":  document.getElementById('Softwarename').value,
-                // "software_image":  document.getElementById('cfgfile').value,
-                "software_is_game": ""+isgame };
-            console.log("[addProducts] data = " + JSON.stringify(data));
-            try {
-                //get json here
-                let newProduct = await $.ajax({
-                    url: "https://cfg-api-ultimate.herokuapp.com/software",
-                    method: "post",
-                    data: JSON.stringify(data),
-                    contentType: "application/json",
-                    dataType: "json"
-                });
-                //
+    alert(""+isgame)
+    let data = {"software_name":  document.getElementById('Softwarename').value,
+        "software_is_game": isgame
+        // "software_image":  document.getElementById('cfgfile').value
+        };
+    console.log("[addProducts] data = " + JSON.stringify(data));
+    try {
+        //get json here
+        let newProduct = await $.ajax({
+            url: "https://cfg-api-ultimate.herokuapp.com/software",
+            method: "post",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json"
+        });
+        //
 
 
-            } catch (err) {
-                console.log(err);
-                if (err.responseJSON) {
-                    alert(""+err.responseJSON.msg);
-                } else {
-                    alert("cfg saved") ;
-                     window.location.href='../dashboard/cfgUpload.html'
-                }
-            }
+    } catch (err) {
+        console.log(err);
+        if (err.responseJSON) {
+            alert(""+err.responseJSON.msg);
+        } else {
+            alert("cfg saved") ;
+             window.location.href='../dashboard/cfgUpload.html'
+        }
+    }
 
 }
