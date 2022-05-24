d2e32c03f50f8fe2e4b76fc5c706646feaf61fd5
Jonhot <jonhot111@gmail.com>
2022-05-24 18:36:00 +0100

    admin hide

--- public/dashboard/js/hide.js
+++ public/dashboard/js/hide.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     let user = sessionStorage.getItem("user_is_admin")
     console.log(user)
-
+    document.querySelector('#test').style.color='yellow'
     const tu = document.querySelector('.hide')
     tu.style.color='magenta'
     tu.style.display = 'none'
