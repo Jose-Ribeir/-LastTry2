59618207dce796b6814021780006036a873d2fab
Jonhot <jonhot111@gmail.com>
2022-05-24 19:16:25 +0100

    admin hide

--- public/dashboard/js/hide.js
+++ public/dashboard/js/hide.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     let user = sessionStorage.getItem("user_is_admin")
     console.log(user)
-    document.querySelector('#test').style.color='yellow'
+
     const tu = document.querySelectorAll('.hide')
 
     for (let i = 0; i < tu.length; i++) {
