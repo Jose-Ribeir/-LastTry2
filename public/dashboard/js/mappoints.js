dfebc79734fe72f68d045adbf38e1afe353947f8
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-25 20:19:01 +0100

    Better css

--- public/dashboard/js/mappoints.js
+++ public/dashboard/js/mappoints.js
@@ -1,3 +1,5 @@
+// Mapa do circlo
+
 const citymap = {
     lisboa: {
         center: { lat: 38.736946, lng: -9.142685 },
@@ -9,27 +11,147 @@ const citymap = {
     },
 };
 
-
-async function initMap() {
-    const json = await getData()
-    console.log(json[1].st_x)
-    console.log(typeof json[1].st_x)
-    var myLatlng = new google.maps.LatLng(parseFloat(json[1].st_x), parseFloat(json[1].st_y));
-
-    var mapOptions = {
-        zoom: 9,
-        center: myLatlng
-    }
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(parseFloat(json[1].st_x), parseFloat(json[1].st_y)),
-        title:"Hello World!"
+async function initMap(a) {
+    // console.log(json)
+    const map = new google.maps.Map(document.getElementById("mapcfg"), {
+        zoom: 13,
+        center: { lat: a.st_x, lng: a.st_y },
+        mapTypeId: "terrain",
+        styles: [
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            {
+                featureType: "administrative.locality",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{ color: "#263c3f" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#6b9a76" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [{ color: "#38414e" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#212a37" }],
+            },
+            {
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry",
+                stylers: [{ color: "#746855" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#1f2835" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#f3d19c" }],
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{ color: "#2f3948" }],
+            },
+            {
+                featureType: "transit.station",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{ color: "#17263c" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#515c6d" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.stroke",
+                stylers: [{ color: "#17263c" }],
+            },
+        ],
     });
 
-    marker.setMap(map);
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the population.
+    // for (const spot in json) {
+    //     // Add the circle for this city to the map.
+    //     const cityCircle = new google.maps.Circle({
+    //         strokeColor: "#FF0000",
+    //         strokeOpacity: 0.8,
+    //         strokeWeight: 2,
+    //         fillColor: "#FF0000",
+    //         fillOpacity: 0.35,
+    //         map,
+    //         center: { lat: spot.st_x, lng: spot.st_y },
+    //         radius: Math.sqrt(spot.sp_view) * 100,
+    //     });
+    // }
 
+
+
+    const cityCircle = new google.maps.Circle({
+        strokeColor: "#ffb500",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#ffb500",
+        fillOpacity: 0.35,
+        map,
+        center: { lat: a.st_x, lng: a.st_y },
+        // center: citymap.lisboa.center,
+        radius: 500,
+    });
 }
+
+
+
+async function getstore(){
+    // var targetUrl = 'https://cfg-api-ultimate.herokuapp.com/users'
+    //
+    //
+    // const response = await fetch(targetUrl)ff
+    // const data = await response.json()
+    // console.log(data)
+    // return data
+
+
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        targetUrl = 'https://cfg-api-ultimate.herokuapp.com/stores'
+    const response = await fetch(
+        targetUrl)
+    const data = await response.json()
+
+    return data
+}
+
+
 
 window.initMap = initMap;
 
