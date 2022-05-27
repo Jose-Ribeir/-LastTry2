abab00221c2224a72027dfba51d2aebbad6a4355
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-27 09:06:22 +0100

    force login when trying to access dashboard

--- public/dashboard/js/changePassword.js
+++ public/dashboard/js/changePassword.js
@@ -1,14 +1,18 @@
 async function changePass1() {
 
     id = sessionStorage.getItem("user_id")
+    let jsno = await getData(id)
 
     alert(""+document.getElementById('newpass2').value === document.getElementById('newpass1').value)
     if (document.getElementById('newpass2').value === document.getElementById('newpass1').value){
+        if (jsno.person_password === document.getElementById('newpass1').value){
+
+
         let data = {"person_id": id,
             "person_password": document.getElementById('oldpass').value,
             "person_passwordnew":  document.getElementById('newpass1').value};
 
-        alert(""+JSON.stringify(data))
+
 
         try {
 
@@ -30,9 +34,23 @@ async function changePass1() {
                 alert(""+err);
             } else {
                 alert("Wrong password") ;
+                }
             }
+        }else {
+            alert("Wrong password")
         }
     }
     else
         alert("Passwords don't coincide")
+}
+
+async function getData(a){
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        targetUrl = linkApi+'users/'+a
+
+    const response = await fetch(
+        targetUrl)
+    const data = await response.json()
+    return data
+
 }
