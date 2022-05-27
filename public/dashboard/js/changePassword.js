a414a9e079d48247e49667efa6245f7891e95712
Jose-Ribeir <91248058+Jose-Ribeir@users.noreply.github.com>
2022-05-27 10:10:02 +0100

    force login when trying to access dashboard

--- public/dashboard/js/changePassword.js
+++ public/dashboard/js/changePassword.js
@@ -1,43 +1,52 @@
 async function changePass1() {
 
     id = sessionStorage.getItem("user_id")
+    let jsno = await getData(id)
 
-
+    alert(""+document.getElementById('newpass2').value === document.getElementById('newpass1').value)
     if (document.getElementById('newpass2').value === document.getElementById('newpass1').value){
-        let data = {"person_id": id,
-            "person_password": document.getElementById('oldpass').value,
-            "person_passwordnew":  document.getElementById('newpass1').value};
+        if (jsno.person_password === document.getElementById('newpass1').value){
 
-        alert(""+JSON.stringify(data))
 
-        try {
-
-            //get json here
-            let newProduct = await $.ajax({
-                url: "https://cfg-api-ultimate.herokuapp.com/changepass",
-                method: "PUT",
-                data: JSON.stringify(data),
-                contentType: "application/json",
-                dataType: "json"
-            });
-            alert(JSON.stringify(newProduct))
+            let data = {"person_id": id,
+                "person_password": document.getElementById('oldpass').value,
+                "person_passwordnew":  document.getElementById('newpass1').value};
 
 
 
-        } catch (err) {
-            alert(""+err);
-            alert("Wrong password") ;
+            try {
 
-            if (err) {
+                //get json here
+                let newProduct = await $.ajax({
+                    url: "https://cfg-api-ultimate.herokuapp.com/changepass",
+                    method: "PUT",
+                    data: JSON.stringify(data),
+                    contentType: "application/json",
+                    dataType: "json"
+                });
+                alert(JSON.stringify(newProduct))
 
-            } else {
-                alert("Passwords Changed")
-                window.location.href='../dashboard/profile.html'
+                window.location.href='../profile.html'
+
+            } catch (err) {
+                console.log(err);
+                if (err) {
+                    alert(""+err);
+                }
             }
+        }else {
+            alert("Wrong password")
         }
     }
-    else{
+    else
         alert("Passwords don't coincide")
-    }
+}
+
+async function getData(a){
+    var targetUrl = linkApi+'users/'+a
+
+    const response = await fetch(targetUrl)
+    const data = await response.json()
+    return data
 
 }
